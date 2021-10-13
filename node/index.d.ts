74ba662f39ab221876559c938afc20400295eded
Alex Hicks <alex@alexhicks.net>
2021-10-13 14:46:24 -0400

    Compile node lib

--- node/index.d.ts
+++ node/index.d.ts
@@ -1,3 +1,5 @@
+export * from "./generated/disk_grpc_pb";
+export * from "./generated/disk_pb";
 export * from "./generated/machine_grpc_pb";
 export * from "./generated/machine_pb";
 export * from "./generated/network_grpc_pb";
--- node/index.js
+++ node/index.js
@@ -10,6 +10,8 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./generated/disk_grpc_pb"), exports);
+__exportStar(require("./generated/disk_pb"), exports);
 __exportStar(require("./generated/machine_grpc_pb"), exports);
 __exportStar(require("./generated/machine_pb"), exports);
 __exportStar(require("./generated/network_grpc_pb"), exports);
